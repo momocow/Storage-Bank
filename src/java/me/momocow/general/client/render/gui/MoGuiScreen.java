2db951bb30491e355b819d01b1f93ab0d9e14765
MomoCow <momocow.me@gmail.com>
2016-12-27 03:41:47 +0800

    GuiIDCard modified

--- src/java/me/momocow/general/client/render/gui/MoGuiScreen.java
+++ src/java/me/momocow/general/client/render/gui/MoGuiScreen.java
@@ -1,27 +1,57 @@
 package me.momocow.general.client.render.gui;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 
-public abstract class MoGuiScreen extends GuiScreen{
+public abstract class MoGuiScreen extends GuiScreen
+{
 	private String unlocalizedName;
 	public int rowHeight = 10;
-	public int guiHeight;
-	public int guiWidth;
+	public int colWidth = 10;
+	protected int guiHeight;
+	protected int guiWidth;
+	protected int offsetX;
+	protected int offsetY;
 	
-	public MoGuiScreen(int w, int h){
-		guiWidth = w;
-		guiHeight = h;
+	public MoGuiScreen(int w, int h, int ox, int oy)
+	{
+		this.guiWidth = w;
+		this.guiHeight = h;
+		this.offsetX = ox;
+		this.offsetY = oy;
 	}
 	
+	public MoGuiScreen(int w, int h)
+	{
+		this(w, h, 0, 0);
+	}
+	
+	/**
+	 * Gui initial size 100x100
+	 */
 	public MoGuiScreen(){
-		this(0, 0);
+		this(100, 100);
 	}
 	
-	public void setUnlocalizedName(String n){
+	public void setOffset(int ox, int oy)
+	{
+		this.offsetX = ox;
+		this.offsetY = oy;
+	}
+	
+	public void setCenter(int cx, int cy)
+	{
+		this.offsetX = cx - this.getGuiWidth() / 2;
+		this.offsetY = cy - this.getGuiHeight() / 2;
+	}
+	
+	public void setUnlocalizedName(String n)
+	{
 		unlocalizedName = "gui." + n;
 	}
 	
-	public String getUnlocalizedName(){
+	public String getUnlocalizedName()
+	{
 		return unlocalizedName;
 	}
 	
@@ -45,28 +75,63 @@ public abstract class MoGuiScreen extends GuiScreen{
 		return this.guiHeight;
 	}
 	
-	public int getRowHeight()
-	{
-		return this.rowHeight;
-	}
-	
 	public int getGlobalX(int guiX)
 	{
-		return ;
+		return this.offsetX + guiX;
 	}
 	
 	public int getGlobalY(int guiY)
 	{
-		return ;
+		return this.offsetY + guiY;
 	}
 	
-	public int getLocalX(int globalX)
+	public int getLocalX(int windowX)
 	{
-		return ;
+		return windowX - this.offsetX;
 	}
 	
-	public int getLocalY(int globalY)
+	public int getLocalY(int windowY)
 	{
-		return ;
+		return windowY - this.offsetY;
 	}
+	
+	public int getCenterX()
+	{
+		return this.offsetX + this.getGuiWidth() / 2;
+	}
+	
+	public int getCenterY()
+	{
+		return this.offsetY + this.getGuiHeight() / 2;
+	}
+	
+	/**
+	 * get the Y coord of the nth row
+	 * @param rowIdx
+	 * @return
+	 */
+	public int row(int rowIdx)
+	{
+		return this.getGlobalY(rowIdx * this.rowHeight);
+	}
+	
+	/**
+	 * get the X coord of the nth column
+	 * @param rowIdx
+	 * @return
+	 */
+	public int col(int colIdx)
+	{
+		return this.getGlobalX(colIdx * this.colWidth);
+	}
+	
+	@Override
+	public void setWorldAndResolution(Minecraft mc, int width, int height)
+    {
+		super.setWorldAndResolution(mc, width, height);
+		
+		//gui size validation
+		this.guiWidth = (this.guiWidth > width)? width: this.guiWidth;
+		this.guiHeight = (this.guiWidth > height)? height: this.guiHeight;
+    }
 }
--- src/java/me/momocow/storagebank/client/render/gui/GuiIDCard.java
+++ src/java/me/momocow/storagebank/client/render/gui/GuiIDCard.java
@@ -1,6 +1,7 @@
 package me.momocow.storagebank.client.render.gui;
 
-import me.momocow.general.client.render.gui.MoGuiScreen;
+import me.momocow.general.client.render.gui.MoCenteredGuiScreen;
+import me.momocow.general.util.LogHelper;
 import me.momocow.storagebank.reference.Reference;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
@@ -9,29 +10,32 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
 
-public class GuiIDCard extends MoGuiScreen{
+public class GuiIDCard extends MoCenteredGuiScreen
+{
 	private final static ResourceLocation TEXTURE = new ResourceLocation(Reference.MOD_ID + ":textures/gui/idcard.png");
 	private final static String NAME = "GuiIDCard";
 	
 	private String textTitle;
+	private String textCardID;
 	private String textOwner;
 	private String textDepository;
 	private String textDeleteDepo;
 	
 	private NBTTagList depoList;
 	private int numDepository;
-	private String textOwnerName;
-	private String textCardID;
+	private String stringOwnerName;
+	private String stringCardID;
 	
 	/**
 	 * Construct the Gui with a bundle of essetial data
 	 */
 	public GuiIDCard(NBTTagCompound data){
-		super(256, 256);
+		super(176, 166);
 		setUnlocalizedName(Reference.MOD_ID + "." + NAME);
 		
 		//gui text init
 		textTitle = I18n.format(getUnlocalizedName() + ".textTitle");
+		textCardID = I18n.format(getUnlocalizedName() + ".textCardID");
 		textOwner = I18n.format(getUnlocalizedName() + ".textOwner");
 		textDepository = I18n.format(getUnlocalizedName() + ".textDepositoryList");
 		textDeleteDepo = "X";
@@ -39,8 +43,8 @@ public class GuiIDCard extends MoGuiScreen{
 		//data collection
 		depoList = (NBTTagList)data.getTag("depoList");
 		numDepository = depoList.tagCount();
-		textCardID = data.getString("cardID");
-		textOwnerName = data.getString("ownerName");
+		stringCardID = data.getUniqueId("cardID").toString();
+		stringOwnerName = data.getString("ownerName");
 	}
 	
 	/**
@@ -68,28 +72,21 @@ public class GuiIDCard extends MoGuiScreen{
     
     public void drawBackgroundLayer()
     {
-    	GlStateManager.color(1.0f, 1.0f, 1.0f);
+    	GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
     	Minecraft.getMinecraft().getTextureManager().bindTexture(GuiIDCard.TEXTURE);
-    	this.drawTexturedModalRect(this.getWindowWidth() / 2 - this.getGuiWidth() / 2, 
-    			this.getWindowHeight() / 2 - this.getGuiHeight() / 2, 
-    			0, 0, this.getGuiWidth(), this.getGuiHeight());
+    	this.drawTexturedModalRect(this.offsetX, this.offsetY, 0, 0, this.getGuiWidth(), this.getGuiHeight());
     }
     
     public void drawForegroundLayer()
     {
-    	int row = 0;
-    	this.drawString(fontRendererObj, textTitle, 
-    			this.getWindowWidth() / 2, 
-    			this.getWindowHeight() / 2 - this.getGuiHeight() / 2 + this.getRowHeight() * row++, 
-    			fontRendererObj.getColorCode('3'));
-    	this.drawString(fontRendererObj, textOwner + ": " + textOwnerName, 
-    			this.getWindowWidth() / 2, 
-    			this.getWindowHeight() / 2 - this.getGuiHeight() / 2 + this.getRowHeight() * row++, 
-    			fontRendererObj.getColorCode('1'));
-    	this.drawString(fontRendererObj, textDepository + ": ", 
-    			this.getWindowWidth() / 2, 
-    			this.getWindowHeight() / 2 - this.getGuiHeight() / 2 + this.getRowHeight() * row++, 
-    			fontRendererObj.getColorCode('2'));
+    	int rowIdx = 1;
+    	
+    	this.drawCenteredString(fontRendererObj, textTitle, this.getCenterX(), this.row(rowIdx), fontRendererObj.getColorCode('1'));
+    	rowIdx += 3;
+    	fontRendererObj.drawSplitString(textCardID + ": " + stringCardID, this.col(1), this.row(rowIdx), this.col(17) - this.col(1), fontRendererObj.getColorCode('0'));
+    	rowIdx += 2;
+    	fontRendererObj.drawString(textOwner + ": " + stringOwnerName, this.col(1), this.row(rowIdx++), fontRendererObj.getColorCode('0'));
+    	fontRendererObj.drawString(textDepository + ": ", this.col(1), this.row(rowIdx++), fontRendererObj.getColorCode('0'));
     }
     
     /**
